ddb19ff5a197959ea48e67cdb8784211c8176959
Flocky <mmoon.official@gmail.com>
2026-09-12 09:29:03 +0300

    added student info and pi calculated

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const http = require('http');
+function calculatePi() {
+  let n = 22;
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += Math.pow(-1, i) / (2 * i + 1);
+  }
+  return sum * 4;
+}
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<h1>Привет, мир!</h1>');
+  const name = 'Шинель Милана Андреевна';
+  const group = '478';
+  const pi = calculatePi();
+  res.end(
+    '<h1>Информация о студенте</h1>' +
+    '<p><strong>ФИО:</strong> ' + name + '</p>' +
+    '<p><strong>Группа:</strong> ' + group + '</p>' +
+    '<p><strong>Число Пи:</strong> ' + pi + '</p>'
+  );
 });
 server.listen(3000, function() {
-  console.log('Сервер запущен: http://localhost:3000');
+  console.log('Сервер на http://localhost:3000');
 });
